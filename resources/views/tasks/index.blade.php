ae75cbe4df72bad6c391577481630f391baeec4b
yuna <ec2-user@ip-172-31-10-122.us-east-2.compute.internal>
2018-06-01 06:34:32 +0000

    bootstrap design fixed2

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -17,7 +17,7 @@
                 @foreach ($tasks as $task)
                     <tr>
                         <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
-                        <td>{{ $task->title }}</td>
+                        <td>{{ $task->status }}</td>
                         <td>{{ $task->content }}</td>
                     </tr>
                 @endforeach
